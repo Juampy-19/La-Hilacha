54690e9e60d6677588bcc6ace2cd35061e6d6802
Juampy-19 <jpcastellano1990@gmail.com>
2024-07-22 15:54:17 -0300

    Realicé el partial del head.ejs y header.ejs

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // Llamando a las rutas.
-const homeRoute = require('./routes/homeRouter.js');
-const pizzasRoute = require('./routes/pizzasRouter.js');
-const empanadasRoute = require('./routes/empanadasRouter.js');
+const homeRoute = require('./routes/homeRouter.js')
+const pizzasRoute = require('./routes/pizzasRouter.js')
+const empanadasRoute = require('./routes/empanadasRouter.js')
 
 // Recursos estaticos.
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Template engine.
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 // Rutas.
-app.use('/', homeRoute);
-app.use('/', pizzasRoute);
-app.use('/', empanadasRoute);
+app.use('/', homeRoute)
+app.use('/', pizzasRoute)
+app.use('/', empanadasRoute)
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}`)
 })
