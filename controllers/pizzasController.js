891f04c9ba3950f73304f87eaa4de2d6f9e0bed7
Juampy-19 <jpcastellano1990@gmail.com>
2024-09-18 14:13:06 -0300

    Implementé un loader en la vista pizzas

--- controllers/pizzasController.js
+++ controllers/pizzasController.js
@@ -2,33 +2,30 @@ const db = require('../database/models')
 
 module.exports = {
   pizzas: (req, res) => {
-    const eternas = db.Pizza.findAll({
-      where: {
-        categoria_id: 1
-      }
-    })
-
-    const clasicas = db.Pizza.findAll({
-      where: {
-        categoria_id: 2
-      }
-    })
-
-    const tradicionales = db.Pizza.findAll({
-      where: {
-        categoria_id: 3
-      }
-    })
-
-    const especiales = db.Pizza.findAll({
-      where: {
-        categoria_id: 4
-      }
-    })
+    const eternas = db.Pizza.findAll({ where: { categoria_id: 1 } })
+    const clasicas = db.Pizza.findAll({ where: { categoria_id: 2 } })
+    const tradicionales = db.Pizza.findAll({ where: { categoria_id: 3 } })
+    const especiales = db.Pizza.findAll({ where: { categoria_id: 4 } })
     Promise
       .all([eternas, clasicas, tradicionales, especiales])
       .then(([eternas, clasicas, tradicionales, especiales]) => {
-        return res.render('pizzas', { eternas, clasicas, tradicionales, especiales })
+        return res.render('pizzas', {
+          eternas,
+          clasicas,
+          tradicionales,
+          especiales,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.error(error)
+        res.render('pizzas', {
+          eternas: [],
+          clasicas: [],
+          tradicionales: [],
+          especiales: [],
+          loading: true
+        })
       })
   }
 }
